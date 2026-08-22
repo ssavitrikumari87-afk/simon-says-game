a00524e5ea361e316bc79fedbca5dfa1557009f5
savitri kumari <ssavitrikumari87@gmail.com>
2026-08-22 16:53:08 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,104 +1,125 @@
-let gameSeq=[];
-let userSeq=[];
 
- let btns=["yellow", "red","purple", "green"];
+if(process.env.NODE_ENV !="production"){
+require('dotenv').config();
+}
+// console.log(process.env.SECRET);
 
- let started=false;
+const express=require("express");
+const app=express();
+const mongoose=require("mongoose");
+// const Listing = require("./models/listing.js");
+const path=require("path");
+const methodOverride=require("method-override");
+const ejsMate=require("ejs-mate");
+// const wrapAsync=require("./utils/wrapAsync.js");
+const ExpressError=require("./utils/ExpressError.js");
 
- let level=0;
- let highScore = 0;
- 
+const session=require("express-session");
 
-    let h2=document.querySelector("h2");
-document.addEventListener("keypress",function(){
-//console.log("game started");
+const flash=require("connect-flash");
+const passport=require("passport");
+const LocalStrategy=require("passport-local");
+const User=require("./models/user.js");
 
-   if(started==false)
-  {
-         console.log("game started");
-       started=true;
 
-       levelUp();
-  }
+// const {listingSchema,reviewSchema}=require("./schema.js");
+// const Review=require("./models/review.js");
+const listingsRouter=require("./routes/listing.js");
+const reviewsRouter=require("./routes/review.js")
+const userRouter=require("./routes/user.js");
+
+
+
+
+const MONGO_URL='mongodb://127.0.0.1:27017/wanderlust';
+
+
+//connect DATABASES
+main().then(()=>{
+    console.log("connected to DB");
+}).catch(err=>{
+    console.log(err);
 });
 
-function gameFlash(btn){
-    btn.classList.add("flash");
-       setTimeout(function(){
-                btn.classList.remove("flash");
-    },250);
- }
+async function main(){
+    await mongoose.connect(MONGO_URL);
+}
 
- function userFlash(btn){
-    btn.classList.add("userFlash");
-       setTimeout(function(){
-                btn.classList.remove("userFlash");
-    },500);
- }
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
 
- function levelUp(){
-   userSeq=[];
-       level++;
-         h2.innerText = `Level ${level}`;
-
-    let randIdx = Math.floor(Math.random()*3);
-
-      let randColor = btns[randIdx];
-   let randBtn = document.querySelector(`.${randColor}`);
-   gameSeq.push(randColor);
-   console.log(gameSeq);
-
-   // console.log(randIdx);
-   //  console.log(randColor);
-   // console.log(randBtn);
-
-   gameFlash(randBtn);
- }
-
- function checkAns(idx){
-   //console.log("curr level:",level);
-   //let idx=level-1;
-   
-   if(userSeq[idx]==gameSeq[idx])
-   {
-      //console.log("same value");
-      if(userSeq.length==gameSeq.length)
-          {
-         setTimeout(levelUp,1000);
-         //levelUp();
-      }
-
-   }else{
-    
-      h2.innerHTML=`Game Over ! Your score was <b>${level}</b><br>Press any key to start.`;
-      document.querySelector("body").style.backgroundColor="red";
-      setTimeout(function(){
-         document.querySelector("body").style.backgroundColor="white";
-      },150);
-      reset();
+//define express-session
+const sessionOptions={
+    secret:"mysupersecretcode",
+    resave:false,
+   saveUninitialized:true,
+   cookie:{
+   expires:Date.now()+ 7 * 24 * 60 * 60 *1000,
+   maxAge:7 * 24 * 60 * 60 *1000,
+   httpOnly:true
    }
+};
 
- }
- function btnPress(){
-   // console.log(this);
-    let btn=this;
-    userFlash(btn);
-    userColor=btn.getAttribute("id");
-   //  console.log(userColor);
-   userSeq.push(userColor);
-   checkAns(userSeq.length-1);
- }
+app.get("/",(req,res)=>{
+    res.send("Hi I am root");
+});
 
- let allBtns=document.querySelectorAll(".btn");
- for(btn of allBtns){
-    btn.addEventListener("click", btnPress);
- }
 
- function reset(){
-   started=false;
-   gameSeq=[];
-   userSeq=[];
-   level=0;
- }
+app.use(session(sessionOptions));
+app.use(flash());
 
- 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req,res,next)=>{
+    res.locals.success=req.flash("success");
+        res.locals.error=req.flash("error");
+//    console.log(res.locals.success);
+
+   res.locals.currUser=req.user;
+    next();
+});
+
+// app.get("/demouser", async (req, res) => {
+//     let fakeUser = new User({
+//         email: "student2@gmail.com",
+//         username: "delta-student2",
+//     });
+
+//     let registeredUser = await User.register(fakeUser, "helloworld");
+
+//     res.send(registeredUser);
+// });
+
+app.use("/listings", listingsRouter);
+app.use("/listings/:id/reviews",reviewsRouter);
+app.use("/",userRouter);
+
+// All routes
+
+
+
+// 404 Route
+app.use((req, res, next) => {
+    next(new ExpressError(404, "Page Not Found!"));
+});
+
+// Error Handler
+app.use((err, req, res, next) => {
+    const { statusCode = 500, message = "Something went wrong!" } = err;
+   res.status(statusCode).render("error.ejs",{message});
+    // res.status(statusCode).send(message);
+});
+
+//start server
+app.listen(3000,()=>{
+    console.log("server is listening to port 3000");
+});
